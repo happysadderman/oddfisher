7335383bf82693c139f3e48a17b05340c0aed462
Allen Choi <choiae0417@gmail.com>
2024-07-05 22:57:20 -0500

    update

--- src/oddfisher.py
+++ src/oddfisher.py
@@ -7,6 +7,9 @@ import argparse
 from pathlib import Path
 from typing import Any
 
+import numpy as np
+import pandas as pd
+
 from scipy.stats import hypergeom as dhyper
 from scipy.optimize import brentq
 
@@ -42,6 +45,26 @@ def construct_2x2(
 
     """
     pass
+
+
+def compute_dnhyper(
+    data: np.ndarray,
+    odd_ratio: float | None = None,
+    is_log: bool = True,
+) -> float:
+    """Compute non-central hypergeomtric distribution parameter."""
+    mn = data.sum(axis=0)
+    m = mn[0]
+    n = mn[1]
+    k = data.sum(axis=1)[0]
+
+    x = data[0][0]
+    lo = max(0, k - n)
+    hi = min(k, m)
+    nval = odd_ratio
+
+    logdc = dhyper(np.arange(lo, hi + 1), m, n, k, log=is_log)
+
 
 def fisher_exact() -> None:
     pass
