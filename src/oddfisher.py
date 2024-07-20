44460f68afece1cbab21577cb657f7ec394adb3f
Allen Choi <choiae0417@gmail.com>
2024-07-20 19:20:11 -0400

    add additional computes

--- src/oddfisher.py
+++ src/oddfisher.py
@@ -66,17 +66,62 @@ def dhyper(
         * def hyper_sf(k, M, n, N): return rmath.lib.phyper(k, n, M-n, N, False, False)
 
     Args:
-        k:
-        M:
-        n:
-        N:
+        k: # of Successes
+        M: Total number of objects
+        n: Total number of Type I objects (Total Positives)
+        N: # of Total Type I object drawn (True Positive)
         is_log:
 
+    Examples:
+        >>> dhyper([0, 1, 2, 3], 10, 3, 4)  # 2x2 in [[1, 3], [2, 4]]
+        array([-1.79175947, -0.69314718, -1.2039728 , -3.40119738])
+    
     """
     if is_log:
         return hypergeom.logpmf(k, M, n, N)
 
     return hypergeom.pmf(k, M, n, N)
+
+
+def compute_dnhyper(
+    support: list[int],
+    logdc: list[float],
+    ncp: int,
+) -> float:
+    """Compute ...
+    
+    Args:
+        ncp: non-centrality parameter, the oddratio
+    
+    >>> compute_dnhyper(np.array([0, 1, 2, 3]), np.array([-1.79175947, -0.69314718, -1.2039728 , -3.40119738]), 10)
+    array([0.00243309, 0.0729927 , 0.4379562 , 0.486618  ])
+
+    """
+    d = logdc + np.log(ncp) * support
+    d = np.exp(d - max(d))
+    return d / np.sum(d)
+
+
+def compute_mnhyper(
+    support: list[int],
+    data: np.ndarray,
+    odd_ratio: int,
+    is_log: bool=True,
+):
+    """Compute mnhyper.
+    
+    Args:
+        support:
+        data:
+        odd_ratio:
+        is_log: 
+    
+    Examples:
+    >>>
+
+    """
+    return np.sum(support * compute_dnhyper(data, odd_ratio, is_log))
+
 
 
 def compute_dnhyper(
@@ -86,16 +131,17 @@ def compute_dnhyper(
 ) -> float:
     """Compute non-central hypergeomtric distribution parameter."""
     mn = data.sum(axis=0)
-    m = mn[0]
-    n = mn[1]
-    k = data.sum(axis=1)[0]
+    m = mn[0]  # Total Type I drawn (FP + TP)
+    n = mn[1]  # FN + TN
+    k = data.sum(axis=1)[0] # TP + FN
 
     x = data[0][0]
     lo = max(0, k - n)
     hi = min(k, m)
     nval = "odd_ratio"
 
-    logdc = dhyper(np.arange(lo, hi + 1), m, n + m, k, log=is_log)
+    logdc = dhyper(np.arange(lo, hi + 1), m + n, k, m, log=is_log)
+
 
 
 def fisher_exact() -> None:
