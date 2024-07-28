7ccca68a475890d2552d924f4ab97bde280a00a9
Allen Choi <choiae0417@gmail.com>
2024-07-28 18:19:35 -0400

    update additional functinos

--- src/oddfisher.py
+++ src/oddfisher.py
@@ -54,7 +54,7 @@ def dhyper(
     N: int,
     is_log: bool = True,
 ) -> float:
-    """Compute non-central hypergeometric distribution H with non-centrality parameter ncp, the odd ratio.
+    """Compute non-central hypergeometric density distribution H with non-centrality parameter ncp, the odd ratio.
     
     Does not work for boundary values for ncp (0, int), but it does not need to.
 
@@ -70,7 +70,7 @@ def dhyper(
         M: Total number of objects (TP + FN + FN + TN)
         n: Total number of Type I objects or has disease (Total Positives, TP + FN)
         N: # of Total Type I object drawn or dignosed as diseased (TP + FP)
-        is_log:
+        is_log: True if in log scale
 
     Examples:
         >>> dhyper([0, 1, 2, 3], 10, 3, 4)  # 2x2 in [[1, 3], [2, 4]]
@@ -81,58 +81,97 @@ def dhyper(
 
 
 def phyper(
-    k: list[int],
+    k: int,
     M: int,
     n: int,
     N: int,
     is_log: bool = True,
+    is_lower_tail: bool = True,
 ) -> float:
+    """Compute hypergeometric distribution H.
+    
+    Args:
+        k: # of Successes (or called diseased)
+        M: Total number of objects (TP + FN + FN + TN)
+        n: Total number of Type I objects or has disease (Total Positives, TP + FN)
+        N: # of Total Type I object drawn or dignosed as diseased (TP + FP)
+        is_log: True if in log scale
+        is_lower_tail: True if probabilities are P[X≤x], otherwise, P[X>x]
+
+    Examples:
+        >>> phyper([0, 1, 2, 3], 10, 3, 4, is_log=True, is_lower_Tail=False)  # 2x2 in [[1, 3], [2, 4]]
+        array([-0.18232156, -1.09861229, -3.40119738,        -inf])
+        >>> phyper([0, 1, 2, 3], 10, 3, 4, is_log=True, is_lower_Tail=True)
+        array([-1.79175947, -0.40546511, -0.03390155,  0.        ])
+        >>> phyper([0, 1, 2, 3], 10, 3, 4, is_log=False, is_lower_Tail=False)
+        array([0.83333333, 0.33333333, 0.03333333, 0.        ])
+        >>> phyper([0, 1, 2, 3], 10, 3, 4, is_log=False, is_lower_Tail=True)
+        array([0.16666667, 0.66666667, 0.96666667, 1.        ])
+
+    """
+    if is_log:
+        if is_lower_tail:
+            return np.log(1 - np.exp(hypergeom.logsf(k, M, n, N)))
+        else:
+            return hypergeom.logsf(k, M, n, N)
+    else:
+        if is_lower_tail:
+            return 1 - hypergeom.sf(k, M, n, N)
+        else:
+            return hypergeom.sf(k, M, n, N)
     
 
-def compute_dnhyper(
+def delete_compute_dnhyper(
     support: list[int],
     logdc: list[float],
-    ncp: int,
+    odd_ratio: int | float,
 ) -> float:
     """Compute ...
     
     Args:
-        ncp: non-centrality parameter, the oddratio
+        odd_ratio: non-centrality parameter, the oddratio
     
     >>> compute_dnhyper(np.array([0, 1, 2, 3]), np.array([-1.79175947, -0.69314718, -1.2039728 , -3.40119738]), 10)
     array([0.00243309, 0.0729927 , 0.4379562 , 0.486618  ])
 
     """
-    d = logdc + np.log(ncp) * support
+    d = logdc + np.log(odd_ratio) * support
     d = np.exp(d - max(d))
     return d / np.sum(d)
 
 
 def compute_mnhyper(
     support: list[int],
-    data: np.ndarray,
-    odd_ratio: int,
-    is_log: bool=True,
-):
+    M: int,
+    n: int,
+    N: int,
+    is_log: bool = True,
+    odd_ratio: int | float = 1,
+) -> float:
     """Compute mnhyper.
     
     Args:
-        support:
-        data:
-        odd_ratio:
-        is_log: 
     
     Examples:
     >>>
 
     """
-    return np.sum(support * compute_dnhyper(data, odd_ratio, is_log))
+    if odd_ratio == 0:
+        return max(0, N - n)
+    elif odd_ratio == np.inf:
+        return min(N, n)
+    return np.sum(support * compute_dnhyper(support, M, n, N, is_log=is_log, odd_ratio=odd_ratio))
 
 
-def compute_phyer(
-    q: int,
-    ncp: int = 1,
-    use_upper_tail: bool = False,  
+def compute_pnhyper(
+    support: list[int],
+    x: int,
+    M: int,
+    n: int,
+    N: int,
+    is_log: bool = True,
+    is_lower_tail: bool = True,
+    odd_ratio: int | float = 1,
 ) -> np.float:
     """Compute phyper.
     
@@ -141,30 +180,116 @@ def compute_phyer(
     Returns:
     
     """
-    if ncp == 1:
+    lo = max(0, N - n)
+    hi = min(N, n)
+
+    if odd_ratio == 1:
+        if not is_lower_tail:
+            x = x - 1
         return phyper(
-            x - 1 if use_upper_tail else x,
-            m,
+            x,
+            M,
             n,
-            k,
-            use_upper_tail=use_upper_tail,
+            N,
+            is_log=is_log,
+            is_lower_tail=is_lower_tail,
         )
     
-    if ncp == 0:
-        return q <= lo if use_upper_tail else q >= lo
+    if odd_ratio == 0:
+        return int(x >= lo if is_lower_tail else x <= lo)
     
-    if ncp == np.inf:
-        return q <= hi if use_upper_tail else q >= hi
+    if odd_ratio == np.inf:
+        return int(x >= hi if is_lower_tail else x <= hi)
     
-    return sum(dnhyper(ncp) * [support >= q] if use_upper_tail else [support <= q])
+    return sum(
+        compute_dnhyper(
+            support,
+            M,
+            n,
+            N,
+            is_log=is_log,
+            odd_ratio=odd_ratio,
+        ) * [
+            support <= x
+        ] if is_lower_tail else [
+            support >= x
+        ]
+    )
 
 
-def compute_dnhyper(  # dhyper?
-    data: np.ndarray,
-    odd_ratio: float | None = None,
+def compute_dnhyper(
+    support: list[int],
+    M: int,
+    n: int,
+    N: int,
     is_log: bool = True,
-) -> float:
-    """Compute non-central hypergeomtric distribution parameter."""
+    odd_ratio: int | float = 1,
+) -> list[float]:
+    """Compute non-central hypergeomtric distribution parameter.
+    
+    """
+    d = dhyper(support, M, n, N, log=is_log) + np.log(odd_ratio) * support
+    d = np.exp(d - max(d))
+    return d / np.sum(d)
+
+
+def get_pvalue(
+    support: list[int],
+    x: int,
+    M: int,
+    n: int,
+    N: int,
+    odd_ratio: int | float,
+    is_log: bool,
+    relError: float = 1 + 10 ** -7,
+) -> tuple[int | float]:
+    """Get p-values."""
+    lo = max(0, N - n)
+    hi = min(N, n)
+
+    if odd_ratio == 0:
+        two_tailed_val = int(x == lo)
+    elif odd_ratio == np.inf:
+        two_tailed_val = int(x == hi)
+    else:
+        d = compute_dnhyper(support, M, n, N, is_log=is_log, odd_ratio=odd_ratio)
+        two_tailed_val = sum(d[d <= d[x - lo + 1] * relError])
+    
+    lower_tail_val = compute_pnhyper(
+        support,
+        x,
+        M,
+        n,
+        N,
+        is_log=is_log,
+        is_lower_tail=True,
+        odd_ratio=odd_ratio,
+    )
+
+    upper_tail_val = compute_pnhyper(
+        support,
+        x,
+        M,
+        n,
+        N,
+        is_log=is_log,
+        is_lower_tail=False,
+        odd_ratio=odd_ratio,
+    )
+
+    return two_tailed_val, lower_tail_val, upper_tail_val
+
+
+
+def fisher_exact(
+    data: np.ndarray,
+    odd_ratio: int | float = 1,
+    is_log: bool = True 
+) -> None:
+    """
+    >>> compute_dnhyper(np.array([[1, 3], [2, 4]]), odd_ratio=10, is_log=True)
+    array([0.00243309, 0.0729927 , 0.4379562 , 0.486618  ])
+    """
     mn = data.sum(axis=0)
     M = sum(mn)
     n = mn[0]  # Total diseased, TP + FN
@@ -174,12 +299,8 @@ def compute_dnhyper(  # dhyper?
     # x = data[0][0]  # TP
     lo = max(0, N - n)
     hi = min(N, n)
-    nval = "odd_ratio"
-
-    logdc = dhyper(np.arange(lo, hi + 1), M, n, N, log=is_log)
-
-
-
-def fisher_exact() -> None:
-    pass
+    # nval = "odd_ratio"
+    support = np.arange(lo, hi + 1)
+    
+    # compute_dnhyper(support, M, n, N, is_log=is_log, odd_ratio=odd_ratio)
 
