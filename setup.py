1b7da020c28fb6f8973644089b05fe575e8587dc
Allen Choi <choiae0417@gmail.com>
2024-09-01 19:07:24 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="oddfisher",
-    version="0.1.1",
+    version="0.1.2",
     description="A simple Python package for fisher exact test with hypothesized odds ratio",
     packages=find_packages(),
     classifiers=[
